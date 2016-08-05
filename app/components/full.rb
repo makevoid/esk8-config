e16b1ea2e6c50e5e657091220f097e2a11164c8c
Francesco 'makevoid' Canessa <makevoid@gmail.com>
2016-08-05 18:49:11 +0100

    add full config groups

--- app/components/full.rb
+++ app/components/full.rb
@@ -10,24 +10,67 @@ class Full
 
   def render
     div(class: "container") {
-      h5 { text "Full configuration" }
+      h6 { text "This is the full VESC configuration, divided by section:" }
 
       if @conf
+        # groups = %i(main limits bldc_sensorless hall_sensors foc pid_control current_control misc)
+        gconf = CONF_KEYS_GROUPS
+
+        # @conf.config.select.with_index{ |c, idx| idx % 3 == 0 }.each { |key, val|
+
         div(class: "row") {
           div(class: "col s12 m4") {
-            @conf.config.select.with_index{ |c, idx| idx % 3 == 0 }.each { |key, val|
+            h5 { text "Main"}
+            @conf.config.select{|k,v| gconf[:main].include? k }.each { |key, val|
               component ConfigInput.new key: key
             }
+            div(class: "s-20")
+
+            h5 { text "Limits"}
+            @conf.config.select{|k,v| gconf[:limits].include? k }.each { |key, val|
+              component ConfigInput.new key: key
+            }
+
+            h5 { text "Current control"}
+            @conf.config.select{|k,v| gconf[:current_control].include? k }.each { |key, val|
+              component ConfigInput.new key: key
+            }
+            div(class: "s-20")
+
           }
           div(class: "col s12 m4") {
-            @conf.config.select.with_index{ |c, idx| idx % 3 == 1 }.each { |key, val|
+            h5 { text "BLDC (sensorless)"}
+            @conf.config.select{|k,v| gconf[:bldc_sensorless].include? k }.each { |key, val|
               component ConfigInput.new key: key
             }
+            div(class: "s-20")
+
+            h5 { text "Sensors"}
+            @conf.config.select{|k,v| gconf[:hall_sensors].include? k }.each { |key, val|
+              component ConfigInput.new key: key
+            }
+            div(class: "s-20")
+
+            h5 { text "PID control"}
+            @conf.config.select{|k,v| gconf[:hall_sensors].include? k }.each { |key, val|
+              component ConfigInput.new key: key
+            }
+            div(class: "s-20")
+
+            h5 { text "Misc"}
+            @conf.config.select{|k,v| gconf[:misc].include? k }.each { |key, val|
+              component ConfigInput.new key: key
+            }
+            div(class: "s-20")
+
           }
           div(class: "col s12 m4") {
-            @conf.config.select.with_index{ |c, idx| idx % 3 == 2 }.each { |key, val|
+            h5 { text "FOC"}
+            @conf.config.select{|k,v| gconf[:foc].include? k }.each { |key, val|
               component ConfigInput.new key: key
             }
+            div(class: "s-20")
+
           }
         }
       end
--- app/seeds.rb
+++ app/seeds.rb
@@ -1,5 +1,113 @@
 # TODO: separate seeds from confs / consts
 
+CONF_KEYS_GROUPS = {
+  main: %w(
+    pwm_mode
+    comm_mode
+    motor_type
+    sensor_mode
+  ),
+  limits: %w(
+    l_current_max
+    l_current_min
+    l_in_current_max
+    l_in_current_min
+    l_abs_current_max
+    l_min_erpm
+    l_max_erpm
+    l_max_erpm_fbrake
+    l_max_erpm_fbrake_cc
+    l_min_vin
+    l_max_vin
+    l_battery_cut_start
+    l_battery_cut_end
+    l_slow_abs_current
+    l_rpm_lim_neg_torque
+    l_temp_fet_start
+    l_temp_fet_end
+    l_temp_motor_start
+    l_temp_motor_end
+    l_min_duty
+    l_max_duty
+  ),
+  bldc_sensorless: %w(
+    sl_min_erpm
+    sl_min_erpm_cycle_int_limit
+    sl_max_fullbreak_current_dir_change
+    sl_cycle_int_limit
+    sl_cycle_int_limit_high_fac
+    sl_cycle_int_rpm_br
+    sl_bemf_coupling_k
+  ),
+  hall_sensors: %w(
+    hall_table_0
+    hall_table_1
+    hall_table_2
+    hall_table_3
+    hall_table_4
+    hall_table_5
+    hall_table_6
+    hall_table_7
+    hall_sl_erpm
+  ),
+  foc: %w(
+    foc_current_kp
+    foc_current_ki
+    foc_f_sw
+    foc_dt_us
+    foc_encoder_inverted
+    foc_encoder_offset
+    foc_encoder_ratio
+    foc_sensor_mode
+    foc_pll_kp
+    foc_pll_ki
+    foc_motor_l
+    foc_motor_r
+    foc_motor_flux_linkage
+    foc_observer_gain
+    foc_duty_dowmramp_kp
+    foc_duty_dowmramp_ki
+    foc_openloop_rpm
+    foc_sl_openloop_hyst
+    foc_sl_openloop_time
+    foc_sl_d_current_duty
+    foc_sl_d_current_factor
+    foc_hall_table_0
+    foc_hall_table_1
+    foc_hall_table_2
+    foc_hall_table_3
+    foc_hall_table_4
+    foc_hall_table_5
+    foc_hall_table_6
+    foc_hall_table_7
+    foc_sl_erpm
+  ),
+  pid_control: %w(
+    s_pid_kp
+    s_pid_ki
+    s_pid_kd
+    s_pid_min_rpm
+    p_pid_kp
+    p_pid_ki
+    p_pid_kd
+    p_pid_ang_div
+  ),
+  current_control: %w(
+    cc_startup_boost_duty
+    cc_min_current
+    cc_gain
+    cc_ramp_step_max
+  ),
+  misc: %w(
+    m_fault_stop_time_ms
+    m_duty_ramp_step
+    m_duty_ramp_step_rpm_lim
+    m_current_backoff_gain
+    m_encoder_counts
+    m_sensor_port_mode
+  ),
+}
+
 CONF_KEYS = %w(
 pwm_mode
 comm_mode
